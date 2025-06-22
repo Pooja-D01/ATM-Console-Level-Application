9ea07073f3dcea48bca950ddbb49653b21595774
Pooja-D01 <poojasuresh8925@gmail.com>
2025-06-22 16:54:32 +0530

    Delete Note2000.java

--- Note2000.java
+++
@@ -1,9 +0,0 @@
-package NotesList;
-
-import Notes.Notes;
-
-public class Note2000 extends Notes {  //Note 2000 class inherits from the notes class
-    public Note2000(long note,long count){  //constructor to initialize the variables
-            super(note,count);  //inherited form the super class
-        }
-}
